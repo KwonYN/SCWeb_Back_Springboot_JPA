54eb47b960b994ede2c876d66cec55c92cfe6f0e
YongnamKwon <rednada2003@naver.com>
2021-03-24 00:45:51 +0900

    2021.03.23. Tue - Configuration

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		// for test : 잘 돌아가는가!?!?
+		Hello hello = new Hello();
+		hello.setStr("hi");
+		String str = hello.getStr();
+		System.out.println("str = " + str);
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
